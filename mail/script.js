8325a933c2b130a93a3ebba61ea8649c8cef15cd
LorisMarzocchi <ml77182a@gmail.com>
2023-04-17 17:34:56 +0200

    addmail

--- mail/script.js
+++ mail/script.js
@@ -6,3 +6,12 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 
 */
+let myArray = ['ciao@gmail.com', 'sadfsd@gmail.com', 'aaa@gmail.com'];
+const myMail = prompt('inserisci la mail: ');
+console.log(myArray);
+if (myMail === (myArray[0]) || myMail === (myArray[1]) || myMail === (myArray[2])){
+    document.getElementById('mail_select').innerHTML = myMail;
+}
+else{
+    document.getElementById('mail_select').innerHTML = 'la mail non esiste';
+}
