1933a8baffab15ef849b688e3cd2ec73777b15f1
LorisMarzocchi <ml77182a@gmail.com>
2023-04-17 17:15:13 +0200

    addgiocodadi

--- gioco dadi/script.js
+++ gioco dadi/script.js
@@ -6,3 +6,20 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 */
 
+const player1 = parseInt(prompt('scegli un numero:'));
+const pc = Math.floor(Math.random() * 6);
+
+document.getElementById('player').innerHTML = player1;
+document.getElementById('computer').innerHTML = pc;
+
+if (player1 > pc){
+    document.getElementById('maggiore').innerHTML = ' Hai vinto';
+
+}
+else if(player1 < pc){ 
+    document.getElementById('maggiore').innerHTML = ' Hai perso';
+}
+else {
+    document.getElementById('maggiore').innerHTML = ' Nessuno, sono uguali';
+
+}
